9219b8cc0715e65243bca4c81b923d8fcb497bf0
liguohang <liguohang@huice.com>
2021-10-28 14:22:10 +0800

    refactor(0.3.0): 将模块从commonJS改为es6module

--- demo.js
+++ demo.js
@@ -1,7 +1,6 @@
-const MarkdownCompiler = require("./lib");
+// const MarkdownCompiler = require("./lib");
+import MarkdownCompiler from './lib/index.js'
 
 const md = new MarkdownCompiler()
-// const tokens = md.parser('# 123\n')
-// console.log(tokens)
 const html = md.render('# 123\n')
 console.log(html)
